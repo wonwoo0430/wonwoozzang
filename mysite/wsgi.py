422395ff5cb4a8e5791fcc06acb7210662392620
wonwoo0430 <wonwoo0430@gmail.com>
2018-12-14 10:50:26 +0900

    hihi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 import sys
 
-path = '/home/<wonwoo0430>/wonwoozzang'  # PythonAnywhere 계정으로 바꾸세요.
+path = '/home/wonwoo0430/wonwoozzang'  # PythonAnywhere 계정으로 바꾸세요.
 if path not in sys.path:
     sys.path.append(path)
 
