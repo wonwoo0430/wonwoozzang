3d065739f5dc10a7bda789a6c48a43b6e4dbb0bf
wonwoo0430 <wonwoo0430@gmail.com>
2018-12-15 16:38:14 +0900

    Hello

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    #url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^$',views.index, name = 'index'),
-
     url(r'^A/$',views.A, name = 'A'),
 url(r'^AA/$',views.AA, name = 'AA'),
 url(r'^AABA/$',views.AABA, name = 'AABA'),
